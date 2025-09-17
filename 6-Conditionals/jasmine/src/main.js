c6e07728120faeca3e57e93492e51e6e3489b2bd
Kaden-Norlander <knorlander26@basecampcodingacademy.org>
2025-09-17 16:02:09 -0500

    6-Conditionals

---
+++ 6-Conditionals/jasmine/src/main.js
@@ -0,0 +1,22 @@
+let kind = [`car`]
+let needLicenseResult = true;
+
+let option1 = `s14`;
+let option2 = `supra`;
+let chooseVehicleResult;
+if (option1 > option2) {
+    chooseVehicleResult = option1 + ` is clearly the better choice.`
+} else {
+    chooseVehicleResult = option2 + ` is clearly the better choice.`
+}
+
+let originalPrice = 40000;
+let age = 32;
+let calculateResellPriceResult;
+    if (age < 3) {
+        calculateResellPriceResult = originalPrice * .8
+    } else if (age > 10) {
+        calculateResellPriceResult = originalPrice * .5
+    } else {
+        calculateResellPriceResult = originalPrice * .7
+    }
