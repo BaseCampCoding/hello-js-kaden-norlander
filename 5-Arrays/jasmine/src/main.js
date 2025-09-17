35f763d51203fdb5bd3826cd5b62a00d53c3ca24
Kaden-Norlander <knorlander26@basecampcodingacademy.org>
2025-09-17 15:29:57 -0500

    4-Boolean and 5-Arrays

--- 5-Arrays/jasmine/src/main.js
+++ 5-Arrays/jasmine/src/main.js
@@ -1,1 +1,28 @@
+let position = 9;
+let stack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let getItemResult = stack[position];
+
+let replacementCard = 11;
+stack[9] = replacementCard;
+
+let newCardTop = 21;
+stack.unshift(newCardTop);
+
+stack.splice(3, 1);
+
+stack.splice(0, 1);
+
+let newCardBottom = 18;
+stack.push(newCardBottom);
+
+stack.splice(10, 1)
+
+let stackSize = stack.length;
+let checkSizeOfStackResult = true; 
+    if (stackSize = false) {
+        checkSizeOfStackResult = false
+    }
+
+console.log(checkSizeOfStackResult)
+
 
